5b53d3d80fbbfb51f3be8a86fd97ce8f3a8fe685
natighajiyev <natig.hajiyev1@gmail.com>
2024-03-01 11:19:48 +0400

    Register Activity & Navigation issues fixed

--- app/src/main/java/com/esrefnifteliyev/cryptoapp/view/activity/RegisterActivity.kt
+++ app/src/main/java/com/esrefnifteliyev/cryptoapp/view/activity/RegisterActivity.kt
@@ -4,14 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.esrefnifteliyev.cryptoapp.R
 import com.esrefnifteliyev.cryptoapp.databinding.ActivityMainBinding
+import com.esrefnifteliyev.cryptoapp.databinding.ActivityRegisterBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class RegisterActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
